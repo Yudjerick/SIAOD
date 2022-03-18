efe410a6829b3eba08b65e83b0164912b578fc88
Yudjerick <91619830+Yudjerick@users.noreply.github.com>
2022-03-18 11:41:43 +0300

    work on 3.1

--- Task3 (strings)/SIAOD3.1(char[])/SIAOD3.1(char[]).cpp
+++ Task3 (strings)/SIAOD3.1(char[])/SIAOD3.1(char[]).cpp
@@ -20,10 +20,19 @@ void AddChar(char* A, char B)
     A[alen] = B;
 }
 
-//Не работаект с вектором, может использовать просто двумерный массив???????
-vector<char*> Words(char* str)
+
+//разобраться с этим
+char** AddWord(char** arr, int &size, char* word)
 {
-    vector<char*> arr;
+    arr = new char* [size + 1];
+    arr[size] = word;
+    return arr;
+}
+
+//Не работаект с вектором, может использовать просто двумерный массив???????
+char** Words(char* str)
+{
+    char** arr;
     char word[100] = "";
     for (int i = 0; i < strlen(str); i++) 
     {
@@ -40,7 +49,6 @@ vector<char*> Words(char* str)
     }
     cout << word << endl;
     word[0] = '\0';
-    //arr.push_back(word);
     return arr;
 }
 
@@ -52,16 +60,25 @@ int main()
     cin.getline(A, 100);
     cin.getline(B, 100);
 
+    
     //Unit(A, B);
     //cout << A << endl;
     //AddChar(A, 'X');
     //cout << A;
 
-    vector<char*> arr = Words(A);
+    /*char fir[1] = "";
+    char** arr = new char*[1];
+    arr[0] = fir;
+    
+    int size = 0;
+    arr = AddWord(arr, size, A);
+    arr = AddWord(arr, size, A);
+    arr = AddWord(arr, size, B);
 
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < size; i++)
     {
-        cout << arr[i];
+        cout << arr[i] << endl;
     }
+    cout << size;*/
 }
 
