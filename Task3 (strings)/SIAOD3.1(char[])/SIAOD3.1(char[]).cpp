508851ccaa02902194da4fecc5b1ab648e480e4c
Yudjerick <denispetrenko2004@gmail.com>
2022-03-31 14:54:36 +0300

    Task 3(char[]) finished

--- Task3 (strings)/SIAOD3.1(char[])/SIAOD3.1(char[]).cpp
+++ Task3 (strings)/SIAOD3.1(char[])/SIAOD3.1(char[]).cpp
@@ -3,16 +3,17 @@
 #include <string.h>
 using namespace std;
 
-void Unit(char* A, char* B) 
+void Unit(char* A, char* B)
 {
     int alen = strlen(A);
     int blen = strlen(B);
-    for (int i = alen; i < alen + blen + 1; i++) 
+    for (int i = alen; i < alen + blen + 1; i++)
     {
         A[i] = B[i - alen];
     }
 }
 
+//добаление символа в конец строки
 void AddChar(char* A, char B)
 {
     int alen = strlen(A);
@@ -21,33 +22,41 @@ void AddChar(char* A, char B)
 }
 
 
-//разобраться с этим
-char** AddWord(char** arr, int &size, char* word)
+/*char** AddWord(char** arr, int& size, char* word)
 {
     arr = new char* [size + 1];
     arr[size] = word;
     return arr;
-}
+}*/
 
-bool Compare(char* A, char* B) 
+//проверяет одинаковы ли строки
+bool Compare(char* A, char* B)
 {
     int i = 0;
     while (true)
     {
-        
-        
+        if (A[i] != B[i]) {
+            return false;
+        }
+        if (A[i] == '\0') {
+            return true;
+        }
+        i++;
     }
 }
 
-//Не работаект с вектором, может использовать просто двумерный массив???????
-void Words(char* str)
+//сравнение поиск данного слова в строке
+bool CompareWords(char* str, char* flag)
 {
     char word[100] = "";
-    for (int i = 0; i < strlen(str); i++) 
+    for (int i = 0; i < strlen(str); i++)
     {
-        if (str[i] == ' ') 
+        if (str[i] == ' ')
         {
-            cout << word << endl;
+            if (Compare(word, flag)) {
+                cout << word << endl;
+                return true;
+            }
             word[0] = '\0';
         }
         else
@@ -55,39 +64,55 @@ void Words(char* str)
             AddChar(word, str[i]);
         }
     }
-    cout << word << endl;
+    if (Compare(word, flag)) {
+        cout << word << endl;
+        return true;
+    }
+    return false;
     word[0] = '\0';
+}
+
+//сравнение слов в строках и печать одинаковых слов
+void Words(char* str, char* str2)
+{
+    int wordsPrinted = 0;
+    char word[100] = "";
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == ' ')
+        {
+            if (word[0] == '\0') {
+                continue;
+            }
+            if (CompareWords(str2, word))
+            {
+                wordsPrinted++;
+            }
+            word[0] = '\0';
+        }
+        else
+        {
+            AddChar(word, str[i]);
+        }
+    }
+    if (CompareWords(str2, word))
+    {
+        wordsPrinted++;
+    }
+    word[0] = '\0';
+    if (wordsPrinted == 0) {
+        cout << "No same words";
+    }
 }
 
 int main()
 {
     char A[100] = "aaaa bbbb cccc";
-    char B[100] = "eeeee dddddd";
+    char B[100] = "eeeee  aaaa dddddd";
 
-    Words(A);
+    cin.getline(A, 100);
+    cin.getline(B, 100);
+    Words(A, B);
 
-    //cin.getline(A, 100);
-    //cin.getline(B, 100);
-
-    
-    //Unit(A, B);
-    //cout << A << endl;
-    //AddChar(A, 'X');
-    //cout << A;
-
-    /*char fir[1] = "";
-    char** arr = new char*[1];
-    arr[0] = fir;
-    
-    int size = 0;
-    arr = AddWord(arr, size, A);
-    arr = AddWord(arr, size, A);
-    arr = AddWord(arr, size, B);
-
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << endl;
-    }
-    cout << size;*/
 }
 
