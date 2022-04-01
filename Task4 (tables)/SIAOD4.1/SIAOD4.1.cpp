e9a2e3e437bb4b1d20645984cd36c954b3747750
Yudjerick <denispetrenko2004@gmail.com>
2022-04-01 20:36:27 +0300

    Almost finish 4.1, need to include header

--- Task4 (tables)/SIAOD4.1/SIAOD4.1.cpp
+++ Task4 (tables)/SIAOD4.1/SIAOD4.1.cpp
@@ -31,7 +31,7 @@ struct Table
     int n;
 };
 
-string DateToS(Date &d) {
+string DateToS(Date& d) {
     string r = "";
     if (d.day < 10) {
         r += "0";
@@ -45,7 +45,7 @@ string DateToS(Date &d) {
     return r;
 }
 
-string TimeToS(Time &t) {
+string TimeToS(Time& t) {
     string r = "";
     if (t.hour < 10) {
         r += "0";
@@ -63,11 +63,11 @@ string TimeToS(Time &t) {
 
 void Print(Table tab)
 {
-	cout << setw(20) << left << "theatre";
-	cout << setw(30) << left << "film";
-	cout << setw(15) << left << "date";
-	cout << setw(15) << left << "time";
-	cout << setw(15) << left << "price"<< endl;
+    cout << setw(20) << left << "theatre";
+    cout << setw(30) << left << "film";
+    cout << setw(15) << left << "date";
+    cout << setw(15) << left << "time";
+    cout << setw(15) << left << "price" << endl;
     for (int i = 0; i < tab.n; i++) {
         cout << setw(20) << left << tab.strings[i].theatre;
         cout << setw(30) << left << tab.strings[i].film;
@@ -85,24 +85,28 @@ Movie MakeMovie() {
 
     float price;
     cin >> theatre >> film >> day >> month >> hour >> minute >> price;
-    Movie m;
-    m.theatre = theatre;
+    Movie m = { theatre, film, {day, month}, {hour, minute}, price };
+    /*m.theatre = theatre;
     m.film = film;
     m.date.day = day;
     m.date.month = month;
     m.time.hour = hour;
     m.time.minute = minute;
-    m.price = price;
+    m.price = price;*/
     return m;
 }
 
-void AddMovie(Movie m, Table &t) 
+void AddMovie(Movie m, Table& t)
 {
-    for (int i = 0; i < t.n; i++) 
+    if (t.n == t.max) {
+        cout << "cannot add more, table is full" << endl;
+        return;
+    }
+    for (int i = 0; i < t.n; i++)
     {
         if (t.strings[i].theatre == m.theatre)
         {
-            for (int j = t.n; j > i; j--) 
+            for (int j = t.n; j > i; j--)
             {
                 t.strings[j] = t.strings[j - 1];
             }
@@ -115,6 +119,46 @@ void AddMovie(Movie m, Table &t)
     t.n++;
 }
 
+Table EmptyTable()
+{
+    Table t;
+    t.n = 0;
+    t.max = 100;
+    return t;
+}
+
+void DeleteStr(Table &t, int pos)
+{
+    for (int i = pos; i < t.n - 1; i++) {
+        t.strings[i] = t.strings[i + 1];
+    }
+    t.n--;
+}
+
+void ClearDate(Table &t)
+{
+    cout << "Enter day and month devided by space:\n";
+    int day;
+    int month;
+    cin >> day >> month;
+    for (int i = 0; i < t.n; i++) {
+        while(t.strings[i].date.day == day && t.strings[i].date.month == month && i < t.n) {
+            DeleteStr(t, i);
+        }
+    }
+}
+
+void WhereWatch(Table t) {
+    cout << "Enter film name:\n";
+    string filmName;
+    cin >> filmName;
+    for (int i = 0; i < t.n; i++) {
+        if (t.strings[i].film == filmName) {
+            cout << t.strings[i].theatre;
+        }
+    }
+}
+
 
 int main()
 {
@@ -123,11 +167,19 @@ int main()
     Movie pirates = { "formula","Pirates", apr5, noon, 400 };
     Movie alladin = { "formula","Alladin", apr5, noon, 300 };
     Movie backToF = { "retro", "BackToTheFuture", {1,5}, {17,40} ,250 };
-	Table afisha;
-    afisha.n = 0;
+    Movie backToF2 = { "retro", "BackToTheFuture2", {7,5}, {17,40} ,250 };
+    Movie backToF3 = { "retro", "BackToTheFuture2", {3,5}, {17,40} ,250 };
+    Movie backToF4 = { "retro", "BackToTheFuture3", {5,4}, {17,30} ,250 };
+    Table afisha = EmptyTable();
     AddMovie(backToF, afisha);
     AddMovie(pirates, afisha);
     AddMovie(alladin, afisha);
-    //AddMovie(MakeMovie(), afisha);
-	Print(afisha);
+    AddMovie(backToF2, afisha);
+    AddMovie(backToF3, afisha);
+    AddMovie(backToF4, afisha);
+ 
+    Print(afisha);
+    ClearDate(afisha);
+    Print(afisha);
+    WhereWatch(afisha);
 }
