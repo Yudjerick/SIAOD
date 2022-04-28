6a179d4c920dc407a307338b8b92a0e3c2aa9cd5
Yudjerick <denispetrenko2004@gmail.com>
2022-04-28 13:02:43 +0300

    Start Task6

---
+++ Task6 (recursion)/SIAOD6.1(cosinus)/SIAOD6.1(cosinus)/SIAOD6.1(cosinus).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int F(int x)
+{
+	if (x <= 1)
+		return 1;
+	return x * F(x - 1);
+}
+
+double Cos(double x, double e) 
+{
+	return 1.1;
+}
+
+int main()
+{
+	
+}
