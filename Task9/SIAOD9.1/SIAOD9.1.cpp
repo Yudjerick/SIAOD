faa728013422d896c275874e68aa8ecfdaa25bf5
Yudjerick <denispetrenko2004@gmail.com>
2022-05-20 11:54:36 +0300

    start task9

---
+++ Task9/SIAOD9.1/SIAOD9.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+struct CityInfo
+{
+    int key;
+    string name;
+};
+
+CityInfo LinearSearch(vector<CityInfo> arr,int key)
+{
+    for (int i = 0; i < arr.size(); i++) 
+    {
+        if (arr[i].key == key) {
+            return arr[i];
+        }
+    }
+}
+
+struct HashTable
+{
+    CityInfo h[1000];
+
+    void Add(CityInfo ci)
+    {
+
+    }
+};
+
+int main()
+{
+    vector<CityInfo> arr = { {495,"Moscow"},{300,"NewYork"},{245,"London"} };
+    cout << LinearSearch(arr,495).name;
+}
